42a51ad3596e2e1f3a6c2b654130a1d32f70a817
DEEPAK <deepakrathore2k1@gmail.com>
2021-12-07 14:15:26 +0530

    updated test

--- test.py
+++ test.py
@@ -16,19 +16,20 @@ def test_list_all_tasks():
     assert len(r.json()["tasks"][0]) == 3
 
 def test_get_task():
-    r = requests.get('http://localhost:5000/v1/tasks/61ae8096cf6208c6cc6e0526')
+    r = requests.get('http://localhost:5000/v1/tasks/61af0ce877777acc41e26417')
     assert isinstance(r.json(),dict)
     # assert isinstance(r.json()["id"], int)
+    print(r.json())
     assert isinstance(r.json()["title"], str)
     assert isinstance(r.json()["is_completed"], bool)
     assert len(r.json()) == 3
 
 def test_update_task():
-    r = requests.put('http://localhost:5000/v1/tasks/61ae8096cf6208c6cc6e0526', json={"title": "My 1st Task", "is_completed": True})
+    r = requests.put('http://localhost:5000/v1/tasks/61af0ce877777acc41e26417', json={"title": "My 1st Task", "is_completed": True})
     assert not r.content
 
 def test_delete_task():
-    r = requests.delete('http://localhost:5000/v1/tasks/61ae8096cf6208c6cc6e0526')
+    r = requests.delete('http://localhost:5000/v1/tasks/61af1f0aabebef6cb2687ed2')
     assert not r.content
 
 test_create_task();
